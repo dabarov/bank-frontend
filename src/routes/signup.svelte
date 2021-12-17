4442b02b80eaea81623b14e8cb7a32187f164038
Aldiyar Dabarov <simlyalde@gmail.com>
2021-12-18 00:39:10 +0600

    feat: add test sign in and sign up pages

---
+++ src/routes/signup.svelte
@@ -0,0 +1,6 @@
+<form action="http://localhost:8080/signup" method="post">
+    <input name="iin" id="iin">
+    <input name="login" id="login">
+    <input name="password" id="password">
+    <button>Sign Up</button>
+</form>
